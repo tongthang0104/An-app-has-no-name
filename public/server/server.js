332e27f8e5c84a29105553680d6e652efe61728c
Thang Tong <tongthang0104@gmail.com>
2016-09-17 14:45:44 -0700

    [feat] Server side hook up on 9999

--- public/server/server.js
+++ public/server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('./mongo.config');
 
+<<<<<<< 81dee0a8c5085efb6b4fab93ae0b23cad215d4dd
 const app = express();
+=======
+
+>>>>>>> [feat] Server side hook up on 9999
 app.use('/', express.static(`${__dirname}/../..`));
 
 app.listen(process.env.PORT || 9999);
