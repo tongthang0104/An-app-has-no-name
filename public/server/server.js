cb3d73816fb341177ca74a7942f823ce07e12069
Thang Tong <tongthang0104@gmail.com>
2016-09-17 16:39:44 -0700

    [feat]update server.js and package.json
    Delete procfile

--- public/server/server.js
+++ public/server/server.js
@@ -1,3 +1,7 @@
-require('babel-register');
+const express = require('express');
+const app = express();
 
-require('./server.babel');
+app.use('/', express.static(`${__dirname}/../..`));
+app.listen(process.env.PORT || 9999);
+
+console.log('Server is running on Port 9999');
