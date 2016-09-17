eea842ca9df48345e20a3dbcbd6fa0eff16e7850
Spencer Baird <spencer.baird36@gmail.com>
2016-09-17 16:28:43 -0700

    (refractor) Refactored mongodbto ES6 syntax

--- public/server/server.babel.js
+++ public/server/server.babel.js
@@ -1,9 +1,27 @@
+"use strict";
 import express from 'express';
 
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+ , assert = require('assert');
 
 app.use('/', express.static(`${__dirname}/../..`));
 
+const mongodb = 'mongodb://localhost/jeopardy';
+const findDocuments = (db, callback) => {
+  const collection = db.collection('medium');
+  collection.find({}).toArray((err, docs) => {
+    assert.equal(err, null);
+    console.log(docs);
+    callback(docs);
+  });
+};
+MongoClient.connect(mongodb, (err, db) => {
+  assert.equal(null, err);
+  findDocuments(db, () => {
+    db.close();
+  });
+});
 
 app.listen(process.env.PORT || 9999);
 
