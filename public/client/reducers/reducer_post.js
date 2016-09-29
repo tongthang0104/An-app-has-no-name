316dc4c2eab5e096e59b3d3a0073f3f4e51b9018
Thang Tong <tongthang0104@gmail.com>
2016-09-28 19:29:32 -0700

    (fix) Clean up

--- public/client/reducers/reducer_post.js
+++ public/client/reducers/reducer_post.js
@@ -2,7 +2,6 @@
 
 import { FETCH_QUESTIONS, FETCH_QUESTIONS_RANDOM, FETCH_MULTI_QUESTIONS } from '../constants/index';
 
-
 export default function(state = null, action) {
   switch(action.type) {
     case FETCH_QUESTIONS:
@@ -12,13 +11,11 @@ export default function(state = null, action) {
 
     case FETCH_QUESTIONS_RANDOM:
       if(action.payload.data){
-        return action.payload.data;
+          return action.payload.data;
       }
 
     case FETCH_MULTI_QUESTIONS:
       if (action.payload) {
-
-        console.log(action.payload)
         return action.payload;
       } else {
         console.log('need data')
