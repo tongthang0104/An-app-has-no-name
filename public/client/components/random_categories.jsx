1675ad9acdb520bcf93de557c57ac9267f2d03ae
Thang Tong <tongthang0104@gmail.com>
2016-09-28 19:28:42 -0700

    (fix) Score reset when switch to Single Player

--- public/client/components/random_categories.jsx
+++ public/client/components/random_categories.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { fetchQuestionsRandCat } from '../actions/index';
+import { fetchQuestionsRandCat, changeScore } from '../actions/index';
 
 class FetchBar extends Component {
 
   constructor(props) {
     super(props);
     this.fetchQuestionsRandCat = this.props.fetchQuestionsRandCat.bind(this);
+    this.reset = this.reset.bind(this);
+    this.changeScore = this.props.changeScore.bind(this);
   }
 
+
+  reset() {
+    this.fetchQuestionsRandCat();
+    this.changeScore(0);
+  }
   render() {
     return (
       <Link to="/play" onClick={this.fetchQuestionsRandCat} className="btn btn-primary btn-lg btn-block navbar" data-loading-text="Loading...">Get Random Categories Instead</Link>
@@ -17,4 +24,4 @@ class FetchBar extends Component {
   }
 }
 
-export default connect(null, {fetchQuestionsRandCat})(FetchBar);
+export default connect(null, {fetchQuestionsRandCat, changeScore})(FetchBar);
