d83c5cf0c646f66323df9781df59a361c97a14c5
Thang Tong <tongthang0104@gmail.com>
2016-10-02 13:53:58 -0700

    (fix) Bug in deploy.js

--- public/server/config/deploy.js
+++ public/server/config/deploy.js
@@ -3,12 +3,12 @@
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
-  const deployConfig = path.resolve(__dirname, '.../../../webpack.production.config.js');
+  const deployConfig = path.resolve(__dirname, '../../../webpack.production.config.js');
   console.log("config", deployConfig);
   // We basically just create a child process that will run
   // the production bundle command
   const child_process = require('child_process');
-  
+
   child_process.exec(`webpack -p --config ${deployConfig}`, function (error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
