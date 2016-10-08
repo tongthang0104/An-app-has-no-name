d909a77c39f8f854edf87b077ee3633279420971
andrewjmeng@gmail.com <andrewjmeng@gmail.com>
2016-10-08 11:18:59 -0700

    (fix) Updated testing to work

---
+++ test/reducer-test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount, render } from 'enzyme';
+import sinon from 'sinon';
+
+
+import reducer from '../public/client/reducers/reducer_active_category';
+import ActionTypes from '../public/client/constants/index'
+
+describe('app reducer', () => {
+  it('initializes the state', () => {
+    expect(reducer(undefined, {})).to.equal(null)
+  })
+
+})
